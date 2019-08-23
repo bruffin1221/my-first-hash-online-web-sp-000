18e690e757b7fd5d8d3b5af688f97080cba2f32e
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-23 21:55:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,9 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  puts shipping_manifest["whale bone corsets"]
+  puts shipping_manifest["porcelain vases"]
+  puts shipping_manifest["oil paintings"]
 
 end
 
@@ -24,14 +26,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
 
 end
