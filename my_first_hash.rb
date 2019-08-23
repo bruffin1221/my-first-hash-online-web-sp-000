cfe0c7642339eb0137db5f2cd0f8a0e0535e7ab4
Bruce Ruffin <bruffin1221@gmail.com>
2019-08-23 22:02:56 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,7 +13,7 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  puts shipping_manifest["oil paintings"]
+  return shipping_manifest["oil paintings"]
 
 end
 
